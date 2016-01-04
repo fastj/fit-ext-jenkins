c0be7945bc6048519a87ae6af94a47c8bc57f7f1
zhouqingquan <zhouqingquan668@163.com>
2016-01-04 21:47:38 +0800

    fit ext for jenkins

---
+++ src/org/fastj/fit/jenkins/JenkinsPostProc.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2015  FastJ
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fastj.fit.jenkins;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.fastj.fit.intf.PostProc;
+import org.fastj.fit.intf.TCNode;
+import org.fastj.fit.intf.TProject;
+import org.fastj.fit.intf.TSuite;
+import org.fastj.fit.log.LogUtil;
+
+/**
+ * Log&Result for Jenkins
+ * 
+ * @author zhouqingquan
+ *
+ */
+public class JenkinsPostProc implements PostProc {
+
+	TProject tproj;
+	long start;
+	long end;
+	boolean filterSkipped = false;
+	
+	List<TCNode> tcDList = new ArrayList<>();
+	Map<String, XMLJUnitResult> emap = new HashMap<>();
+	TSNode totalCnt = new TSNode();
+	
+	@Override
+	public void end() {
+		end = System.currentTimeMillis();
+		
+		for (XMLJUnitResult xjr : emap.values())
+		{
+			xjr.endTestSuite();
+			xjr.count(totalCnt);
+		}
+		
+		FileOutputStream glog = null;
+		try {
+			glog = new FileOutputStream(tproj.getLogFile("fit.log"));
+			glog.write(LogUtil.getLog());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (glog != null)
+			{
+				try {
+					glog.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		
+		System.out.println("<=== End test at " + new Date());
+		
+		System.out.println("------------------------Report-------------------------\r\n");
+		
+		System.out.println("Total run: " + totalCnt.runCount());
+		System.out.println("     PASS: " + (totalCnt.runCount() - totalCnt.failureCount()));
+		System.out.println("     FAIL: " + totalCnt.failureCount());
+		System.out.println("     SKIP: " + totalCnt.skipCount());
+		
+		System.out.println("\r\nAll test takes: " + (end - start)/1000. + " sec.");
+		
+	}
+
+	@Override
+	public void start(TProject tproj) {
+		this.tproj = tproj;
+		start = System.currentTimeMillis();
+		filterSkipped = tproj.getSysVars().gcontains("ignoreSkipped");
+		System.out.println("===> Start test at " + new Date());
+	}
+
+	@Override
+	public void start(TCNode tcn) {
+		String tsname = tcn.getSuite().getName();
+		synchronized (emap) {
+			if (!emap.containsKey(tsname)) {
+				emap.put(tsname, new XMLJUnitResult());
+				emap.get(tsname).startTestSuite(tproj, tcn.getSuite(), tcn.getStartTime());
+			}
+		}
+	}
+	
+	@Override
+	public void finish(TCNode tcn) {
+		if (tcn.getResult() == TCNode.SKIPPED)
+		{
+			totalCnt.setSkipCnt(totalCnt.skipCount() + 1);
+		}
+		
+		if (filterSkipped && tcn.getResult() == TCNode.SKIPPED){
+			return;
+		}
+		
+		tcDList.add(tcn);
+		//ExtPoint: Set Result to 3rd Systems
+		//ExtPoint: Write TestCase Log to 3rd Systems
+		
+		String tsname = tcn.getSuite().getName();
+		emap.get(tsname).appendNode(tcn);
+		
+		TSNode tsn = emap.get(tsname).node;
+		tsn.setEnd(tcn.getEndTime());
+		tsn.setRunCnt(tsn.runCount() + 1);
+		
+		if (tcn.getResult() != TCNode.PASS)
+		{
+			tsn.setFailureCnt(tsn.failureCount() + 1);
+		}
+	}
+
+	@Override
+	public void finish(TSuite suite) {
+		//Can write log when TSuite done
+	}
+
+}
---
+++ src/org/fastj/fit/jenkins/XMLJUnitResult.java
@@ -0,0 +1,164 @@
+package org.fastj.fit.jenkins;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.fastj.fit.intf.TCNode;
+import org.fastj.fit.intf.TProject;
+import org.fastj.fit.intf.TSuite;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class XMLJUnitResult implements Constants{
+
+    private static final double ONE_SECOND = 1000.0;
+    private static final String UNKNOWN = "unknown";
+
+    private Document doc;
+    private Element rootElement;
+    private OutputStream out;
+    protected TSNode node;
+    private File tsRlt = null;
+
+    public void startTestSuite(TProject tproj, TSuite suite, long time) {
+    	node = new TSNode();
+    	node.setName(suite.getName());
+    	node.setStart(time);
+    	
+        doc = domBuilder().newDocument();
+        rootElement = doc.createElement(TESTSUITE);
+        String n = suite.getName();
+        rootElement.setAttribute(ATTR_NAME, n == null ? UNKNOWN : n);
+
+        final String timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date(node.getStartTime()));
+        rootElement.setAttribute(TIMESTAMP, timestamp);
+        rootElement.setAttribute(HOSTNAME, getHostname());
+        
+        try {
+        	tsRlt = tproj.getLogFile(suite.getName().replace('.','_') + "_rlt.xml");
+			out = new FileOutputStream(tsRlt);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+    }
+
+    private String getHostname()  {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "localhost";
+        }
+    }
+
+    public void count(TSNode cnter)
+    {
+    	cnter.setRunCnt(cnter.runCount() + node.runCount());
+    	cnter.setFailureCnt(cnter.failureCount() + node.failureCount());
+    }
+    
+    public void endTestSuite() {
+    	TSNode suite = node;
+    	
+        rootElement.setAttribute(ATTR_TESTS, "" + suite.runCount());
+        rootElement.setAttribute(ATTR_FAILURES, "" + suite.failureCount());
+        rootElement.setAttribute(ATTR_ERRORS, "" + suite.errorCount());
+        rootElement.setAttribute(ATTR_SKIPPED, "" + suite.skipCount());
+        rootElement.setAttribute(ATTR_TIME, "" + (suite.getRunTime() / ONE_SECOND));
+        if (out != null) {
+            Writer wri = null;
+            try {
+                wri = new BufferedWriter(new OutputStreamWriter(out, "UTF8"));
+                wri.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n");
+                (new DOMElementWriter()).write(rootElement, wri, 0, "  ");
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            } finally {
+                if (wri != null) {
+                    try {
+                        wri.flush();
+                    } catch (IOException ex) {
+                        // ignore
+                    }
+                    
+					try {
+						out.close();
+					} catch (IOException e) {
+					}
+                    
+                }
+            }
+        }
+    }
+
+	public void appendNode(TCNode test) {
+		Element currentTest = doc.createElement(TESTCASE);
+		String n = test.getName();
+		currentTest.setAttribute(ATTR_NAME, n == null ? UNKNOWN : n);
+		currentTest.setAttribute(ATTR_CLASSNAME, test.getSuite().getName());
+		rootElement.appendChild(currentTest);
+		formatOutput(currentTest, test.getResult() == TCNode.PASS ? SYSTEM_OUT : SYSTEM_ERR, test.getLog());
+
+		if (test.getResult() != TCNode.PASS)
+		{
+			Element fe = doc.createElement(FAILURE);
+			fe.setAttribute(ATTR_TYPE, "ERROR");
+			currentTest.appendChild(fe);
+		}
+		
+		currentTest.setAttribute(ATTR_TIME, "" + ((test.getEndTime() - test.getStartTime()) / ONE_SECOND));
+	}
+
+    private void formatOutput(Element p, String type, String output) {
+        Element nested = doc.createElement(type);
+        p.appendChild(nested);
+        nested.appendChild(doc.createCDATASection(output));
+    }
+    
+    private DocumentBuilder domBuilder() {
+        try {
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        } catch (Exception exc) {
+            throw new ExceptionInInitializerError(exc);
+        }
+    }
+
+}
+
+interface Constants {
+    String TESTSUITES = "testsuites";
+    String TESTSUITE = "testsuite";
+    String TESTCASE = "testcase";
+    String ERROR = "error";
+    String FAILURE = "failure";
+    String SYSTEM_ERR = "system-err";
+    String SYSTEM_OUT = "system-out";
+    String ATTR_PACKAGE = "package";
+    String ATTR_NAME = "name";
+    String ATTR_TIME = "time";
+    String ATTR_ERRORS = "errors";
+    String ATTR_FAILURES = "failures";
+    String ATTR_TESTS = "tests";
+    String ATTR_SKIPPED = "skipped";
+    String ATTR_TYPE = "type";
+    String ATTR_MESSAGE = "message";
+    String PROPERTIES = "properties";
+    String PROPERTY = "property";
+    String ATTR_VALUE = "value";
+    String ATTR_CLASSNAME = "classname";
+    String ATTR_ID = "id";
+    String TIMESTAMP = "timestamp";
+    String HOSTNAME = "hostname";
+}
+
